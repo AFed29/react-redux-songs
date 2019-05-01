7d70930060850484444e8c9bb04426951908725d
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2019-05-01 15:41:29 +0100

    mapped state to props in songlist component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class SongList extends Component {
   render(){
+    console.log(this.props);
     return <div>SongList</div>
   }
 }
 
-export default SongList;
+const mapStateToProps = (state) => {
+  return { songs: state.songs };
+}
+
+// connect returns a function, which in turn is called with the SongList arg
+export default connect(mapStateToProps)(SongList);
