eb930e22ae9af7b84429f73eca06f351364c5e43
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2019-05-01 15:27:47 +0100

    small fix to reducers, imported and used Provider too:

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,10 @@ import { combineReducers } from 'redux'
 // Reducers
 const songsReducer = () => {
   return [
-    { title: 'No Scrubs', duration "4:05" },
-    { title: 'Macarena', duration "5:22" },
-    { title: 'All Star', duration "3:10" },
-    { title: 'I Want it That Way', duration "2:53" }
+    { title: 'No Scrubs', duration: "4:05" },
+    { title: 'Macarena', duration: "5:22" },
+    { title: 'All Star', duration: "3:10" },
+    { title: 'I Want it That Way', duration: "2:53" }
   ];
 };
 
